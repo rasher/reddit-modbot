46b41158cc00c818bf6d400ffb6711936817f67f
Jonas Häggqvist <rasher@rasher.dk>
2012-09-28 00:14:21 +0200

    Housekeeping. Add license text and .gitignore

--- modbot.py
+++ modbot.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
+
+# Copyright (c) 2012, Jonas Häggqvist <rasher@rasher.dk>
+# All rights reserved.
 #
-# Benefits:
-# * relatively simple rules setup
-# * runs continously, so no startup-penalty
-# * will only consider an item once
-# Drawbacks:
-# * built with single-subreddit usage in mind
-# * less flexible rules
-# * all rules must be written in regex (including stuff like vote counts)
-# * Always reads 100 modqueue items (and never more)
-# * will only consider an item once
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the program nor the names of its contributors may be
+#   used to endorse or promote products derived from this software without
+#   specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 from datetime import datetime
 from pprint import pprint
